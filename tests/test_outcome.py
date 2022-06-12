9f116690465cbbbb896c6fec6eaea4bd2cb924a7
marshall7m <44485531+marshall7m@users.noreply.github.com>
2022-06-12 12:25:53 -0700

    add testing valid outcomes for teardown and setup

--- tests/test_outcome.py
+++ tests/test_outcome.py
@@ -1,8 +1,78 @@
-def test_valid_outcomes(base_tester):
+def test_valid_setup_outcome(base_tester):
     base_tester.makepyfile(
         """
         import pytest
-        import os
+
+
+        @pytest.fixture
+        def foo():
+            yield None
+
+        def test_a(foo):
+            pass
+
+        @pytest.mark.regex_dependency('test_a', target="function", allowed_outcomes=["passed"])
+        def test_b():
+            pass
+    """
+    )
+
+    result = base_tester.runpytest("--verbose")
+    result.assert_outcomes(passed=2, skipped=0, failed=0)
+    print(f"stderr: {result.stdout}")
+    result.stdout.re_match_lines(
+        r"""
+        .*::test_a PASSED
+        .*::test_b PASSED
+    """
+    )
+
+
+def test_invalid_setup_outcome(base_tester):
+    base_tester.makepyfile(
+        """
+        import pytest
+
+
+        @pytest.fixture
+        def foo():
+            pytest.fail()
+            yield None
+
+        def test_a(foo):
+            pass
+
+        @pytest.mark.regex_dependency('test_a', target="function", allowed_outcomes=['passed'])
+        def test_b():
+            pass
+
+        @pytest.mark.regex_dependency('test_a', target="function", allowed_outcomes=["skipped"])
+        def test_c():
+            pass
+
+        @pytest.mark.regex_dependency('test_a', target="function", allowed_outcomes=["passed", "skipped"])
+        def test_d():
+            pass
+    """
+    )
+
+    result = base_tester.runpytest("--verbose")
+    result.assert_outcomes(passed=0, skipped=3, errors=1)
+    result.stdout.re_match_lines(
+        r"""
+        .*::test_a ERROR
+        .*::test_b SKIPPED
+        .*::test_c SKIPPED
+        .*::test_d SKIPPED
+    """
+    )
+
+
+def test_valid_call_outcome(base_tester):
+    base_tester.makepyfile(
+        """
+        import pytest
+
 
         def test_a():
             pass
@@ -38,11 +108,11 @@ def test_valid_outcomes(base_tester):
     )
 
 
-def test_invalid_outcomes(base_tester):
+def test_invalid_call_outcome(base_tester):
     base_tester.makepyfile(
         """
         import pytest
-        import os
+
 
         def test_a():
             pass
@@ -66,6 +136,78 @@ def test_invalid_outcomes(base_tester):
     result.stdout.re_match_lines(
         r"""
         .*::test_a PASSED
+        .*::test_b SKIPPED
+        .*::test_c SKIPPED
+        .*::test_d SKIPPED
+    """
+    )
+
+
+def test_valid_teardown_outcome(base_tester):
+    base_tester.makepyfile(
+        """
+        import pytest
+
+
+        @pytest.fixture
+        def foo():
+            yield None
+            pytest.skip()
+
+        def test_a(foo):
+            pass
+
+        @pytest.mark.regex_dependency('test_a', target="function", allowed_outcomes=["skipped", "passed"])
+        def test_b():
+            pass
+    """
+    )
+
+    result = base_tester.runpytest("--verbose")
+    result.assert_outcomes(passed=2, skipped=1, failed=0)
+    result.stdout.re_match_lines(
+        r"""
+        .*::test_a PASSED
+        .*::test_a SKIPPED
+        .*::test_b PASSED
+    """
+    )
+
+
+def test_invalid_teardown_outcome(base_tester):
+    base_tester.makepyfile(
+        """
+        import pytest
+
+
+        @pytest.fixture
+        def foo():
+            yield None
+            pytest.fail()
+
+        def test_a(foo):
+            pass
+
+        @pytest.mark.regex_dependency('test_a', target="function", allowed_outcomes=['passed'])
+        def test_b():
+            pass
+
+        @pytest.mark.regex_dependency('test_a', target="function", allowed_outcomes=["skipped"])
+        def test_c():
+            pass
+
+        @pytest.mark.regex_dependency('test_a', target="function", allowed_outcomes=["passed", "skipped"])
+        def test_d():
+            pass
+    """
+    )
+
+    result = base_tester.runpytest("--verbose")
+    result.assert_outcomes(passed=1, skipped=3, errors=1)
+    result.stdout.re_match_lines(
+        r"""
+        .*::test_a PASSED
+        .*::test_a ERROR
         .*::test_b SKIPPED
         .*::test_c SKIPPED
         .*::test_d SKIPPED
